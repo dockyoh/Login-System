a029e038582455b162e87b60efb669381514d960
Reygin <sreygin@gmail.com>
2026-05-24 15:48:04 +0800

    Login system UI design

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
@@ -17,11 +18,22 @@
     </header>
     <main>
         <div class="login-container">
+            <h2>Login</h2>
             <form action="" method="post">
-
+                <input type="text" name="in-username" id="" placeholder="Username">
+                <input type="password" name="in-password" id="" placeholder="Password">
+                <button type="submit">Login</button>
             </form>
         </div>
-
+        <div class="signup-container">
+            <h2>Signup</h2>
+            <form action="" method="post">
+                <input type="text" name="up-username" id="" placeholder="Username">
+                <input type="password" name="up-password" id="" placeholder="Password">
+                <input type="email" name="up-email" id="" placeholder="Email">
+                <button type="submit">Signup</button>
+            </form>
+        </div>
     </main>
     <footer></footer>
 </body>
